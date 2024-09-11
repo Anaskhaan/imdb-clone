323b916dc8836132f5a920f747d9beede937a270
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2024-09-12 04:31:13 +0500

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Results from "@/Components/Results";
+import { resolve } from "styled-jsx/css";
 
 const API_KEY = process.env.API_KEY;
 
@@ -7,7 +8,8 @@ export default async function Home({ searchParams }) {
   const res = await fetch(
     `https://api.tmdb.org/3${
       genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
-    }?api_key=${API_KEY}& language=en-US&page1`
+    }?api_key=${API_KEY}& language=en-US&page1`,
+    { next: { revalidate: 10000 } }
   );
   const data = await res.json();
   if (!res.ok) {
